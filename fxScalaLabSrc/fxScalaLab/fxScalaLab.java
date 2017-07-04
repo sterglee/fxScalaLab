c7b99dd8da40f6a217cf38dff9c789ef34d6d103
sterg <sterg@teiemt.gr>
2017-07-04 13:10:56 +0300

    UI upgrade

--- fxScalaLabSrc/fxScalaLab/fxScalaLab.java
+++ fxScalaLabSrc/fxScalaLab/fxScalaLab.java
@@ -9,10 +9,12 @@ import javafx.stage.Stage;
 
 import static org.fxmisc.richtext.demo.JavaKeywords.*;
 import org.fxmisc.richtext.demo.JavaKeywordsAsync;
+import scalaExec.Interpreter.GlobalValues;
 
 
 
 public class fxScalaLab extends Application {
+    
    @Override
    public void start(Stage stage) throws Exception {
     //   String prefix = "/home/sterg/JavaFX/JavaFXSceneBuilder2.0/app/ScalaLab/";
@@ -22,13 +24,24 @@ public class fxScalaLab extends Application {
       Scene scene = new Scene(root); // attach scene graph to scene
          scene.getStylesheets().add(JavaKeywordsAsync.class.getResource("java-keywords.css").toExternalForm());
    
-      stage.setTitle("ScalaLab"); // displayed in window's title bar
+        String TITLE= buildTitle();
+ 
+      stage.setTitle(TITLE); // displayed in window's title bar
       stage.setScene(scene); // attach scene to stage
       
             
       stage.show(); // display the stage
        
    }
+
+ static public String buildTitle() {
+     String buildDate = "4 July 2017"; 
+      String mainFrameTitle =        "fxScalaLab:    Scala "+scala.tools.nsc.Properties.versionString()+",  library type: "+ GlobalValues.interpreterType()+ 
+                   ",   "+System.getProperty("java.vm.name", "").toLowerCase()+",  "+ System.getProperty("os.name", "").toLowerCase()+
+                   "  "+ System.getProperty("os.arch", "").toLowerCase()+" ,   "+ buildDate;
+                   
+      return mainFrameTitle;
+}
 
    public static void main(String[] args) {
 
